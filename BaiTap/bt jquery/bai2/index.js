c814adee96f4176348d6b11f2b7a5fcb021f772d
KayJuno <dangdan2807@gmail.com>
2020-12-11 22:15:03 +0700

    add: bai 1, 2in 'bt jquery'

---
+++ BaiTap/bt jquery/bai2/index.js
@@ -0,0 +1,7 @@
+function test() {
+    $("div > h1").css("background", "#0686c9");
+    $("div p").css("font-size", "20px");
+    $(".something").css("margin", "50px");
+    $("#something").css("color", "red");
+    $("div p#something a").css("text-decoration", "none");
+}
